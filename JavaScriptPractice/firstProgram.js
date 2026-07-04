e6ef54d801933cfcf85f34689074e1afb410ae29
himansu2906 <himaanshuy66@gmail.com>
2026-07-04 23:43:44 +0800

    Added JS file

---
+++ JavaScriptPractice/firstProgram.js
@@ -0,0 +1,45 @@
+// ********************Variable 
+
+// const myName = "Himanshu Yadav"
+// console.log(myName)
+// let url = "what am i doing now"
+// console.log(url);
+
+// *****************data type
+// ***************Array
+
+// let user = ["first","second","third"]
+// console.log(user)
+// console.log(user[0])
+
+// *************Object creation
+// const userdata = {
+//     username : "himanshu",
+//     age : 32,
+//     location : "Singapore"
+// }
+
+// console.log(userdata.age)
+
+// ************function creatoion
+
+// function myTest(){
+//     console.log("himanshu")
+// }
+
+// myTest()
+
+// **************Arrow Function
+// let newArrowFunction = () =>{
+//     console.log("inside arrow function");
+// }
+
+// newArrowFunction()
+
+// ************* Arrow function with parameter
+
+const myName = (name) =>{
+    console.log(`Hi ${name}`)
+}
+
+myName("himanshu")
